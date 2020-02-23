7556ad9d60460409fe99e1cdaca6ec489560136b
industriousparadigm <dsgmcosta@gmail.com>
2020-02-23 11:56:28 +0000

    updated dependencies + crapp cleanup

--- src/App.js
+++ src/App.js
@@ -32,13 +32,13 @@ function App() {
     <React.Fragment>
       <h1>
         Pokémons
-  
+
 </h1>
 
       <p>
         <a href="https://en.wikipedia.org/wiki/List_of_Pok%C3%A9mon">The Pokémon franchise</a> revolves around 832 fictional species of collectible monsters, each having unique designs and skills. Conceived by Satoshi Tajiri in early 1989, Pokémon are creatures that inhabit the fictional Pokémon World.
         This is the list of the first 150 Pokémon as they appear in Pokémon Stadium, starting with Bulbasaur in the top left corner and ending with Mewtwo in the bottom right corner.
-        
+
 </p>
       <div className="container">
 
@@ -52,11 +52,11 @@ function App() {
                 <h3>{pokemon.name}</h3>
                 <p>
                   {pokemon.evolutions && (pokemon.evolutions.length !== 0) && <p> Evolutions:
-          
+
 {pokemon.evolutions.map((e, indx) => {
 
-                      return <p key={indx}> {e.name} </p>
-                    })}
+                    return <p key={indx}> {e.name} </p>
+                  })}
                   </p>}
 
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
@@ -20,10 +19,4 @@ const client = new ApolloClient({
 })
 
 
-ReactDOM.render(<ApolloProvider client={client}><App /></ApolloProvider>, document.getElementById('root'));
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<ApolloProvider client={client}><App /></ApolloProvider>, document.getElementById('root'))
